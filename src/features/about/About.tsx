33cc3ed037b5825e0d3b2fa42f406ef100f61484
Cydo <cydoentis@gmail.com>
2025-01-29 16:55:28 -0500

    Fix: See my work button was not scrolling to the my work section.

--- src/features/about/About.tsx
+++ src/features/about/About.tsx
@@ -1,21 +1,29 @@
+import useNavigation from "@/components/navigation/hooks/useNavigation";
 import Section from "@/components/section/Section";
 import { Box, Button, Group, Title } from "@mantine/core";
 
 function About() {
+  const { activeRouteHandler } = useNavigation({
+    sectionIds: ["about-section", "my-work-section"],
+  });
+
   return (
     <Section id="about-section" withDivider={false} spacing={0}>
-      <Box
-        w={{ base: "100%", md: 460 }}
-      >
+      <Box w={{ base: "100%", md: 460 }}>
         <Title c="text" fw="400" size="2.2rem">
-          Passionate Full-Stack Developer turning challenges into seamless
-          solutions
+          Passionate Full-Stack Developer turning challenges into seamless solutions
         </Title>
         <Group gap={8} pt={32}>
           <Button variant="filled" bg="inverse" c="primary" radius="md">
             Let's talk
           </Button>
-          <Button variant="outline" radius="md" color="inverse">
+          <Button
+            variant="outline"
+            radius="md"
+            color="inverse"
+            onClick={() => activeRouteHandler("my-work-section")}
+
+          >
             See my work
           </Button>
         </Group>
